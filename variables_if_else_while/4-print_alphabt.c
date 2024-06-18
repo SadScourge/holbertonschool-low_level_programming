979d163956cc0cdc2234c5fd31c92a13e90db0f8
SadScourge <darklzar@gmail.com>
2024-06-18 19:37:08 +0200

    file 4 init

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,13 @@ int main(void)
 {
 	char ch = 'a';
 
-	for (ch = a;
-	putchar(a);
+	for (ch = 'a'; ch < 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+		putchar(ch);
+		}
+	}
+	putchar('\n');
 	return (0);
 }
