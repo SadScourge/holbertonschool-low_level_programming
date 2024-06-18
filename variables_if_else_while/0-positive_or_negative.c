189635c1b3368dd2658d82b6115e319355e1a411
SadScourge <darklzar@gmail.com>
2024-06-18 09:38:54 +0200

    file 0 init

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Check if n is superior, equal or inferior
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	r = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive");
+	}
+	else if (n == 0)
+	{
+		printf("is zero");
+	}
+	else
+	{
+		printf("is negative");
+	}
+	return (0);
+}
