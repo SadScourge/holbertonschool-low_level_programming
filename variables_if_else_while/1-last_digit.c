3181cc47328b5acb0424ce876e74b1679570defa
SadScourge <darklzar@gmail.com>
2024-06-18 11:26:16 +0200

    file 1 init

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - give a random number
+ *and say if it is superior or inferior to 5 or equal to 0
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int m;
+
+	m = n % 10;
+	if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, m)
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, m)
+	{
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m)
+	}
+	return (0);
+}
