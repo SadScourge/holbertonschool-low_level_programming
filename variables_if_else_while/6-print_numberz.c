298d57ac87415210cf85d7f8d9c9347754e3390d
SadScourge <darklzar@gmail.com>
2024-06-18 23:44:08 +0200

    file 6 init

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print digit 0 to 9
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch  <= 57; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
