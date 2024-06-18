e3df42285f1b732aeb68cdb86d822aae773a672d
SadScourge <darklzar@gmail.com>
2024-06-18 09:58:11 +0200

    forgot quotes

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive");
+		printf(""%d" is positive");
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero");
+		printf(""%d" is zero");
 	}
 	else
 	{
-		printf("%d is negative");
+		printf(""%d" is negative");
 	}
 	return (0);
 }
