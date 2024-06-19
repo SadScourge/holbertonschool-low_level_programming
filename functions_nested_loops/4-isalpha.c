d9659e98e1ed6fb6cab004c3fda82673ec2d8165
SadScourge <darklzar@gmail.com>
2024-06-19 16:56:09 +0200

    file 4 init and main.h modification

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - check if the char is lowercase
+ * @c: the value to check
+ * Return: return 1 if lower, 0 otherwise
+ */
+int _islower(int c)
+{
+	if ((c > 97 && c < 122)  || (c > 65 && c < 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
