552e33c8a66323588bb05a322151152e4c58ffb3
SadScourge <darklzar@gmail.com>
2024-06-20 01:09:20 +0200

    file 7 init and main modification

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - compute the absolute value
+ * @n: initial value
+ * Return: modulo ten of n
+ */
+int print_last_digit(int n);
+{
+	return (n % 10);
+}
