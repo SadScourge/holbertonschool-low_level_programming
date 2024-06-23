1812b4606582ca355d44db17b0ab5b8010e2260f
SadScourge <darklzar@gmail.com>
2024-06-23 10:13:21 +0200

    betty style for file 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,10 +2,11 @@
 
 /**
  * add - add two int and return sum
- *
+ * @a: first value
+ * @b: second value
  * Return: sum of two int
  */
 int add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
