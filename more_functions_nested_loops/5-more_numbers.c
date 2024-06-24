f2a5d9bcac03c28a408e41736b6bbd80ada5186a
SadScourge <darklzar@gmail.com>
2024-06-24 13:58:08 +0200

    file 5 init

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *more_numbers - print digit from 0 to 9 except 2 & 4
+ */
+void more_numbers(void)
+{
+	int n;
+	int d;
+
+	for (d = 1; d <= 10; d++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar(n / 10);
+			}
+			_putchar(n % d);
+		}
+	_putchar('\n');
+	}
+}
