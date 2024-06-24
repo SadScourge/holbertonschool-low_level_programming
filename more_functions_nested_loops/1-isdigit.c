bdbe42bd6083cf161f38488879a79a31f5007835
SadScourge <darklzar@gmail.com>
2024-06-24 10:28:59 +0200

    file 1 init and main update

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isupper - check if the char is a digit
+ *@c: char to check
+ *Return: 1 if c is a digit
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
