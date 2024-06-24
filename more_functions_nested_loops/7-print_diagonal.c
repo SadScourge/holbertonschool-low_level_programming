1f5fa1bcceb9ef6c037267103d4c1042670c2104
SadScourge <darklzar@gmail.com>
2024-06-24 16:50:28 +0200

    file 7 init

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_diagonal - print a diagonal n times
+ *@n: numbers of time to print \
+ */
+void print_diagonal(int n)
+{
+	int ligne;
+	int espace;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (ligne = 0; ligne < n; ligne++)
+		{
+			for (espace = 0; espace < ligne; espace++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
+
