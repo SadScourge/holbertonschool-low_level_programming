6a0923fac45628256a28e8df5ec3a68f24bdf8cd
SadScourge <darklzar@gmail.com>
2024-06-30 19:15:17 +0200

    file 2 update

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,16 @@ char *_strncpy(char *dest, char *src, int n)
 	char *start = dest;
 	int count = 0;
 
-	while (count < n)
+	while ((count < n) && (*src != '\0'))
 	{
 		*dest = *src;
 		dest++;
 		src++;
 		count++;
+	}
+	if (count < n)
+	{
+		dest = '\0';
 	}
 	return (start);
 }
