cadda3ec616cc0d5be754533f23e82823afe3dea
SadScourge <darklzar@gmail.com>
2024-06-30 20:39:42 +0200

    file 4 init

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverse an array of int
+ *@a: array to reverse
+ *@n: size of the array
+ */
+void reverse_array(int *a, int n)
+{
+	int index;
+	char temp;
+
+	for (index = 0; index < n / 2; index++)
+	{
+		temp = a[index];
+		a[index] = a[n - index - 1];
+		a[n - index - 1] = temp;
+	}
+}
