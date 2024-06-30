732c2880b1bcd67e2dd5f45bc051e4f27e0877db
SadScourge <darklzar@gmail.com>
2024-06-30 20:59:46 +0200

    file 5 init

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *string_toupper - put all lowercase characters into uppercase
+ *@str: the string to modify
+ *Return: always 0
+ */
+char *string_toupper(char *str)
+{
+	while (*str != '\0')
+	{
+		if ((*str >= 97) && (*str <= 122))
+		{
+			*str = *str - 32;
+		}
+		str++;
+	}
+	return (0);
+}
