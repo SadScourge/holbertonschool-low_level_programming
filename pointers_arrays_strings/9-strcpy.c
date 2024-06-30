5c8e982fa484f0fe608d03c9cb9a992515b402d2
SadScourge <darklzar@gmail.com>
2024-06-30 15:15:18 +0200

    file 9 init

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strcpy - copy the string in src to dest
+ *@src: the original string
+ *@dest: the copied string
+ *return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
