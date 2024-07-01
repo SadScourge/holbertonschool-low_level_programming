8e3ce351ecaa9a7b57c24e8facecc1f9c9b8013f
SadScourge <darklzar@gmail.com>
2024-07-01 15:07:08 +0200

    file 0 init

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_memset - set (n) first bytes of an memory area
+*@n: numbers of bytes to set
+*@s: start of the memory area
+*@b: constant byte
+*Return: start of the memory area
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *start = s;
+	unsigned int index = 0;
+
+	while (index < n)
+	{
+		*s = b;
+		s++;
+		index++;
+	}
+	return (start);
+}
