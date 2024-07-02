aad8bb16560e5c09f5c0fc24675240687635bbae
SadScourge <darklzar@gmail.com>
2024-07-02 15:23:10 +0200

    file 3 init

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,27 @@
 #include "main.h"
 /**
- *
- *
+ *_strspn - get the length of a prefix substring
+ *@s: string to analyse
+ *@accept: list of character to count
+ *Return: count of char found
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    int count = 0;
-    char *start = accept;
+	int count = 0;
+	char *start = accept;
 
-    while (*s != ' ')
-    {
-        while(*accept != '\0')
-        {
-            if (*s == *accept)
-            {
-                count++;
-            }
-            accept++;
-        }
-        s++;
-        accept = start;
-    }
-    return (count);
+	while (*s != ' ')
+	{
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+			{
+				count++;
+			}
+			accept++;
+		}
+		s++;
+		accept = start;
+	}
+	return (count);
 }
