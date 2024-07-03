c8e9dc9a08db5133d1757f0fa9f3510003881cc4
SadScourge <darklzar@gmail.com>
2024-07-03 10:19:41 +0200

    file 1 init

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_memcpy - copy the first memory area to the second
+*@n: numbers of bytes to copy
+*@src: first memory area
+*@dest: second memory area
+*Return: start of the dest memory area
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *start = dest;
+	unsigned int index = 0;
+
+	while (index < n)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+		index++;
+	}
+	return (start);
+}
