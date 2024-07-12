9bdce433f6e742c7d5b377eef8d79ee724305916
SadScourge <darklzar@gmail.com>
2024-07-12 23:21:44 +0200

    file 4 update

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+char *_strcpy(char *src);
+int _strlen(char *s);
 
 /**
  **new_dog - create a new dog and allocate space for it
@@ -11,44 +13,72 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int length1, length2, index;
 	dog_t *newdog;
+	char *newdog_name;
+	char *newdog_owner;
 
-	if (name == NULL || age == 0.0 || owner == NULL)
-		return (NULL);
-	while (name[length1] != '\0')
-		length1++;
-	while (name[length2] != '\0')
-		length2++;
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 	{
 		free(newdog);
 		return (NULL);
 	}
-	(*newdog).name = malloc(length1 * sizeof((*newdog).name));
-	if ((*newdog).name == NULL)
+	newdog_name = _strcpy(name);
+	if (newdog_name == NULL)
 	{
-		free((*newdog).name);
+		free(newdog_name);
 		free(newdog);
 		return (NULL);
 	}
-	for (index = 0; index <= length1; index++)
+	newdog_owner = _strcpy(owner);
+	if (newdog_owner == NULL)
 	{
-		(*newdog).name[index] = name[index];
+		free(newdog_owner);
+		free(newdog_name);
+		free(newdog);
+		return (NULL);
 	}
+	(*newdog).name = newdog_name;
 	(*newdog).age = age;
-	(*newdog).owner = malloc(length2 * sizeof((*newdog).owner));
-	if ((*newdog).owner == NULL)
-	{
-		free((*newdog).owner);
-		free((*newdog).name);
-		free(newdog);
-		return (NULL);
-	}
-	for (index = 0; index <= length2; index++)
-	{
-		(*newdog).owner[index] = owner[index];
-	}
+	(*newdog).owner = newdog_owner;
 	return (newdog);
+}
+
+/**
+ *_strcpy - copy the string in src to dest
+ *@src: the original string
+ *@dest: the copied string
+ *Return: the pointer to dest
+ */
+char *_strcpy(char *src)
+{
+int i;
+int len;
+char *dest;
+
+len = _strlen(src);
+dest = malloc(sizeof(char) * len + 1);
+if (dest == NULL)
+return (NULL);
+
+for (i = 0; src[i] != '\0'; i++)
+dest[i] = src[i];
+dest[i] = '\0';
+return (dest);
+}
+
+/**
+ * _strlen - Calculates the length of a string
+ * @s: string
+ * Descripion: Calculates the lenght of a string
+ * Return: An integer representing the lenght of a string
+ */
+int _strlen(char *s)
+{
+int len;
+
+for (len = 0; s[len]; len++)
+{}
+
+return (len);
 }
