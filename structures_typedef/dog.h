d0b1e383697319b7e3b2a705803012c788681789
SadScourge <darklzar@gmail.com>
2024-07-12 15:52:15 +0200

    header update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+struct dog;
 int _putchar(int c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -14,7 +15,7 @@ void print_dog(struct dog *d);
  *@owner: owner of the dog
  *Description: this new type will keep information about dogs
 */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
