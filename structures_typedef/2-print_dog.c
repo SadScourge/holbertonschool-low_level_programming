75e31c64699558604972fecf8b89e196cc2b01eb
SadScourge <darklzar@gmail.com>
2024-07-12 21:32:36 +0200

    file 2 init

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *print_dog - print info about a dog
+ *@d: the struct containing information a bout the dog
+ */
+void print_dog(struct dog *d)
+{
+	if ((*d).name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", (*d).name);
+	}
+	if ((*d).age == 0.0)
+	{
+		printf("age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", (*d).age);
+	}
+	if ((*d).name == NULL)
+	{
+		printf("owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", (*d).owner);
+	}
+}
