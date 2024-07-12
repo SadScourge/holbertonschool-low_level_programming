0d1a7870c22a1c4d78778905e5c8b5e50a706571
SadScourge <darklzar@gmail.com>
2024-07-12 15:35:04 +0200

    header  betty style comment

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -4,14 +4,21 @@
 int _putchar(int c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);*/
+/*dog_t *new_dog(char *name, float age, char *owner);*/
+/*void free_dog(dog_t *d);*/
 
+/**
+ *struct dog - contain information about dogs
+ *@name: name of a dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *Description: this new type will keep information about dogs
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
