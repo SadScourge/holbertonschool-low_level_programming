a08ab391df4ab3bd05018609159fd187679f6650
SadScourge <darklzar@gmail.com>
2024-08-04 18:45:28 +0200

    corrected output if head is NULL

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,11 +15,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
-	{
 		return (NULL);
-	}
 	node->n = n;
-
+	if (*h == NULL)
+	{
+		node->next = NULL;
+		node->prev = NULL;
+		*h = node;
+		return (node);
+	}
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	if (current)
